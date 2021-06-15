e9c22e71573c409b0a290321e18cfa0f233b8a28
f.hamdi <f.hamdi@eksad.com>
2021-06-15 15:18:14 +0700

    routing & firebase

--- src/components/FirebaseProvider.js
+++ src/components/FirebaseProvider.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 
 import firebase from 'firebase/app';
 import 'firebase/auth';
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Login(){
+function Login() {
 
     return <h1>Halaman Login</h1>
 }
